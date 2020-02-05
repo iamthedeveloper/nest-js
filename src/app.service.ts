302e14d4961f3fb97becbba94a89fe73031b59b3
Kranti chintala <iamthedeveloper@users.noreply.github.com>
2020-02-04 23:54:56 -0500

    updated Return JSON

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!' + new Date();
+  getHello(): any {
+    return {
+      'From Nest App' : new Date()
+    }
   }
 }
